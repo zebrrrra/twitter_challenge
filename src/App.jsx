b6af63b3ac6c3b5148540895f594422d96df413a
DESKTOP-NNV32A0\admin <jenalin512@gmail.com>
2023-06-08 23:36:08 +0800

    fix: App.jsx, feat: add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import "./style/main.scss"
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage'
+//import FollowPage from "./pages/FollowPage/FollowPage";
 
 // 先用來測試css有沒有正常
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <Router>
+        <Routes>
+      <Route path ="/" element={<HomePage/>}/>
+
+      {/*<Route path ="/login" element={<LoginPage/>/>
+        <Route path ="/profile" element={<ProfilePage/>}/>
+         <Route path ="/setting" element={<SettingPage/>}/>
+          <Route path ="/" element={<HomePage/>}/> */}
+      </Routes>
+      </Router>
     </div>
   );
 }
