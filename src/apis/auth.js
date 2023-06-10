05d505004eed1be11fe41312833df637e84a526b
DESKTOP-NNV32A0\admin <jenalin512@gmail.com>
2023-06-10 14:46:18 +0800

    fix: resolve commit

---
+++ src/apis/auth.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+const authURl = 'https://tranquil-basin-75437.herokuapp.com';
+export const login = async ({ email, password }) => {
+  try {
+    const { data } = await axios.post(`${authURl}/api/users/login`, { email, password })
+    return data
+  } catch (err) {
+    console.error('[Login Failed]:', err)
+  }
+
+}
