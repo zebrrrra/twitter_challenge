82308b99d7467b565da9931290d4a750df13bfb0
DESKTOP-NNV32A0\admin <jenalin512@gmail.com>
2023-06-16 19:12:44 +0800

    feat: add new recommendList

---
+++ src/components/FollowRecommendList/FollowRecommendList.jsx
@@ -0,0 +1,54 @@
+import style from './RecommendList.module.scss';
+import RecommendItem from '../RecommendItem/RecommendItem';
+import {useState, useEffect} from 'react'; 
+import {getTopFollowers} from '../../apis/user';
+import {postFollowShips, deleteFollowShips } from '../../apis/followship';
+//import {useAuth} from '../../context/AuthContext';
+import { async } from 'q';
+
+ const FollowRecommendList = ()=>{
+    const [users, setUsers] = useState([]);
+
+    const handleFollow = async (id) =>{
+      const response = await postFollowShips(id);
+      if (response && response.status ==='success'){
+        setUsers (users.map(user=>user.id ===id?{...user, isCurrentUserFollowed:true}:user));
+    
+    console.log('handlefollow isCurrentUserFollowed:',id)
+    }
+  };
+  const handleunFollow = async (id) =>{
+    const response = await deleteFollowShips(id);
+    console.log(id) //測試
+    if (response && response.status ==='success'){
+      setUsers (users.map(user=>user.id ===id?{...user, isCurrentUserFollowed:false}:user));
+      console.log('Unfollow isCurrentUserFollowed:',id)
+    }
+};
+
+    useEffect(()=>{
+        const fetchTopFollowers = async () => {
+      
+            const userData =await getTopFollowers();
+            console.log (userData);//測試
+            if (userData) {
+                setUsers(userData);
+            }
+        }
+        fetchTopFollowers();
+    },[]);
+    return (
+    <div className={style.recommendListContainer}>
+      <h5 className="justify center">推薦跟隨</h5>
+      {users.map(user => (
+        <RecommendItem 
+        key={user.id} 
+        user={user}
+        onFollow={handleFollow}
+        onUnfollow={handleunFollow} />
+      ))}
+    </div>
+  )
+};
+
+export default FollowRecommendList;
